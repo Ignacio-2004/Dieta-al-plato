9c6c95013f884d5b1d690f9bde79166806c4054c
DanielMB97 <danielmbemp@gmail.com>
2025-05-10 19:49:44 +0200

    Layout de comidas

--- app/src/main/java/com/tfg/dietaalplato/DiasActivity.java
+++ app/src/main/java/com/tfg/dietaalplato/DiasActivity.java
@@ -1,6 +1,8 @@
 package com.tfg.dietaalplato;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class DiasActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void onClickReturn(View view){
+        Intent intent = new Intent(this, InicioUsuarioActivity.class );
+        startActivity(intent);
     }
 }
